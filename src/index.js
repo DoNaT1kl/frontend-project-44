0070d40370ba5e3cb67c3b40aa2b1aeb87f4d864
Vlad <vlad777888555@mail.ru>
2023-05-08 23:56:48 +0300

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import readlineSync from "readline-sync";
+
+//Приветствие 
+const greeting = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
+};
+
+//Генерация случайного целого числа
+const getRandomNum = (min, max) => {
+    return Math.floor(Math.random() * max) + min;
+};
+
+export { greeting, getRandomNum };
