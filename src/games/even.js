cd9c9a698a245ee1ef58f764e2da21752ae48d9f
Vlad <vlad777888555@mail.ru>
2023-05-08 23:55:09 +0300

    Fix

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,11 @@
+import { greeting, getRandomNum } from '../index.js';
 import readlineSync from "readline-sync";
 
 const isEvenNum = () => {
-//Приветствие
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-//Генерация случайного целого числа
-    const getRandomInt = (min, max) => {
-        return Math.floor(Math.random() * max) + min;
-    };
+//Приветствие
+    const Username = greeting();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 //Проверка на четное число
     const isEven = (num) => num % 2 === 0;
@@ -18,28 +13,28 @@ const isEvenNum = () => {
 //Проверка случайного целого числа на четность
     for (let i = 0; i < 3; i += 1) {
 
-        const randomInt = getRandomInt(1, 100);
-        const answer = readlineSync.question(`Question: ${randomInt} `);
+        const randomInt = getRandomNum(1, 100);
+//Вопрос
+        console.log(`Question: ${randomInt} `)
+        const answer = readlineSync.question('Your answer: ');
         const evenNum = isEven(randomInt);
     
         if (answer === 'yes' && evenNum === true) {
-            console.log('Your answer: yes');
             console.log('Correct!');
         } else if (answer === 'no' && evenNum === false) {
-            console.log('Your answer: no');
             console.log('Correct!');
         } else if (answer === 'yes' && evenNum === false) {
-            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-            console.log(`Let's try again, ${name}!`);
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+            console.log(`Let's try again, ${Username}!`);
             break;
         } else {
-            console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-            console.log(`Let's try again, ${name}!`);
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
+            console.log(`Let's try again, ${Username}!`);
             break;
         }
 
         if ((i + 1) % 3 === 0) {
-            console.log(`Congratulations, ${name}!`);
+            console.log(`Congratulations, ${Username}!`);
           }
     }
 };
