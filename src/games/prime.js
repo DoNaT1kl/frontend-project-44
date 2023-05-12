fe92fe340cd6137c10d99e58b93e84ca25c18a23
Vlad <vlad777888555@mail.ru>
2023-05-12 18:27:12 +0300

    Fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,21 @@ import readlineSync from "readline-sync";
 const Username = greeting();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
+const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+        if (num % i === 0) return false;
+      }
+      return num !== 1;
+    }
+
 const primeGame = () => {
 
-    const isPrime = (num) => {
-        for (let i = 2; i < num; i += 1) {
-            if (num % i === 0) return false;
-          }
-          return num !== 1;
-        }
-        
     for (let i = 0; i < 3; i += 1) {
 
-        const randomInt = getRandomNum(1, 100);
+        const randomNum = getRandomNum(1, 100);
         console.log(`Question: ${randomInt}`);
         const answer = readlineSync.question('Your answer: ');
-        const primeNum = isPrime(randomInt);
+        const primeNum = isPrime(randomNum);
 
         if (answer === 'yes' && primeNum === false) {
             console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${Username}!`);
