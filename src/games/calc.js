20c768596a236e7a8b3bda8aefb3622cad7033bb
Vlad <vlad777888555@mail.ru>
2023-05-12 17:21:49 +0300

    Fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -33,7 +33,7 @@ const calcGame = () => {
         const correctAnswer = calc();
         const answer = readlineSync.question('Your answer: ')
 
-        if (answer == correctAnswer) {
+        if (answer === String(correctAnswer)) {
             console.log('Correct!');
         } else {
             console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${Username}!`)
