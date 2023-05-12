a002567afd48abd75ec5f7836f6eca95ee5b2eec
Vlad <vlad777888555@mail.ru>
2023-05-12 16:47:52 +0300

    Fix games

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,16 +1,17 @@
 import { greeting, getRandomNum } from '../index.js';
 import readlineSync from "readline-sync";
 
+const operators = ['+', '-', '*'];
+const randomIndex = Math.floor(Math.random() * (operators.length));
+const operator = operators[randomIndex];
+
 const calc = () => {
     const Username = greeting();
     console.log('What is the result of the expression?');
 
     for (let i = 0; i < 3; i += 1) {
-        const operators = ['+', '-', '*'];
         const firstNum = getRandomNum(1, 20);
         const secondNum = getRandomNum(1, 20);
-        const randomIndex = Math.floor(Math.random() * (operators.length));
-        const operator = operators[randomIndex];
 
         console.log(`Question: ${firstNum} ${operator} ${secondNum}`);
         const answer = readlineSync.question('Your answer: ')
--- src/games/even.js
+++ src/games/even.js
@@ -1,13 +1,13 @@
 import { greeting, getRandomNum } from '../index.js';
 import readlineSync from "readline-sync";
 
+const isEven = (num) => num % 2 === 0;
+
 const isEvenNum = () => {
 
     const Username = greeting();
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    const isEven = (num) => num % 2 === 0;
-    
     for (let i = 0; i < 3; i += 1) {
 
         const randomInt = getRandomNum(1, 100);
